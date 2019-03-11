6c149a67c871aa8074a92374ac91309b95b1b3da
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-11 13:45:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 def countdown_with_sleep(number)
-  sleep 0.25
+  sleep 5
 end
-
+ 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
-    countdown_with_sleep(number)
-    number -=1
-  end
-    return "HAPPY NEW YEAR!"
+  puts "#{number} SECOND(S)!"
+  countdown_with_sleep(number)
+  number -=1
+end
+return "HAPPY NEW YEAR!"
 end
