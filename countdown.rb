0121cecfc6cd24a03bf49e16f2f043c109f578a5
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-11 13:52:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown_with_sleep(number)
+  sleep 5
+end
 
-def countdown
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  countdown_with_sleep(number)
+  number -=1
+end
+return "HAPPY NEW YEAR!"
 end
